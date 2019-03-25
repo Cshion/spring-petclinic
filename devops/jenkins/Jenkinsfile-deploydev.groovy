2bb9dde44cec248961bbbba3cfdf74bb00e8a1b7
Cshion <aaron.stbn@gmail.com>
2019-03-24 19:02:10 -0500

    test build maven

--- devops/jenkins/Jenkinsfile-deploydev.groovy
+++ devops/jenkins/Jenkinsfile-deploydev.groovy
@@ -1,2 +1,34 @@
 @Library('devopslib')
 import org.cshion.Utils;
+
+def devopsUtils = new Utils(this);
+
+node{
+    stage("Configuration"){
+
+    }
+
+    stage("Build"){
+        utils.buildMaven();
+    }
+
+    stage("Test"){
+
+    }
+
+    stage("QA Analysis"){
+
+    }
+
+    stage("Security Analysis"){
+
+    }
+
+    stage("Upload Artifact"){
+
+    }
+
+    stage("Deploy to Dev Env"){
+
+    }
+}
